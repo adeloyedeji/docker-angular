ffcfd212fe22ff55c84ea6d57f39d3131ad5875f
Matsumu Monday <jereco11@gmail.com>
2019-05-30 09:39:38 +0100

    Firebase credentials for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCpOPNUulYG2QYamkvkI0g3qhxSF7Rf8HU',
+    authDomain: 'dockerangular-a086d.firebaseapp.com',
+    databaseURL: 'https://dockerangular-a086d.firebaseio.com',
+    projectId: 'dockerangular-a086d',
+    storageBucket: 'dockerangular-a086d.appspot.com',
+    messagingSenderId: '157383330930',
+    appId: '1:157383330930:web:fd561bad3bef6ead'
+  }
 };
